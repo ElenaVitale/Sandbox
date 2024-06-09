f112b509e877d61c22370126c8c12bacb5113150
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-09 10:47:28 +1000

    update to do items with relevant answers as per prac3

--- PythonII/prac_03/string_formatting.py
+++ PythonII/prac_03/string_formatting.py
@@ -21,8 +21,8 @@ print("My guitar: " + name + ", first made in " + str(year))
 print("My guitar: {}, first made in {}".format(name, year))
 print("My guitar: {0}, first made in {1}".format(name, year))
 print("My {0} was first made in {1} (that's right, {1}!)".format(name, year))
-
-# And with f-string formatting, introduced in Python 3.6 (do this)
+#
+# # And with f-string formatting, introduced in Python 3.6 (do this)
 print(f"My {name} was first made in {year} (that's right, {year}!)")
 
 # Formatting currency (grouping with comma, 2 decimal places):
@@ -32,7 +32,7 @@ print(f"My {name} would cost ${cost:,.2f}")  # preferred f-string version
 # Aligning columns by using width after the :
 # This loop uses enumerate, which is useful when you want both the index and value
 numbers = [1, 19, 123, 456, -25]
-
+#
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
@@ -54,3 +54,6 @@ print(f"{year} {name} for about ${cost:,.0f}!")
 # 2 ^ 8 is  256
 # 2 ^ 9 is  512
 # 2 ^10 is 1024
+
+for i in range(0, 11):
+    print(f"2 ^{i:>2} is {2 ** i:>4}")
