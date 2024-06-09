89f790985b7b96b787825dd1543e55952264eae4
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-09 11:28:33 +1000

    added out_file to write a file called c_conrad to add the final price details

--- PythonII/prac_03/capitalist_conrad.py
+++ PythonII/prac_03/capitalist_conrad.py
@@ -35,4 +35,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
     price *= (1 + price_change)
     print(f"On day {number_of_days} price is: ${price:,.2f}")
 
-print(f"Final price is: ${price:,.2f}")
+out_file = open('c_conrad', 'w')
+print(f"Final price is: ${price:,.2f}", file=out_file)
