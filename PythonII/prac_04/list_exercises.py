b51fc91c79a25f9fe4938ca1ef3d34fe5f9b8861
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-15 21:28:53 +1000

    basic prac program working

--- PythonII/prac_04/list_exercises.py
+++ PythonII/prac_04/list_exercises.py
@@ -2,11 +2,27 @@
 Write a program that prompts the user for 5 numbers and then stores each of these in a list called numbers.
 The program should then output information about these numbers, as in the output below.
 You can use the functions min, max, sum and len, and you can use the append method to add a number
+
+*** # Notes and has
 """
 
-numbers = []
-for count in range(5):
-    number = float(input(f"Enter number {count+1}: "))
-    numbers.append(number)
 
-print(f"The number's are: {numbers}")
+def main():
+    get_numbers()
+    display_numbers()
+
+def get_numbers():
+    global numbers
+    numbers = []
+    for count in range(5):
+        number = int(input(f"Enter number {count + 1}: "))
+        numbers.append(number)
+
+def display_numbers():
+    print(f"The first number is: {numbers[0]} ")
+    print(f"The last number is: {numbers[-1]} ")
+    print(f"The smallest number is: {min(numbers)}")
+    print(f"The largest number is: {max(numbers)}")
+    print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
+
+main()
