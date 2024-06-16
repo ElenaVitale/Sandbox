3101da035d8ee61721238ad4dcd461c5bc337191
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-16 17:48:16 +1000

    finished program

--- PythonII/prac_04/quick_picks.py
+++ PythonII/prac_04/quick_picks.py
@@ -15,9 +15,26 @@ Your code should produce output that matches this sample output
 
 import random
 
+#Constants
+NUMBERS_PER_LINE = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
 
-quick_pick = []
 
-number_of_picks = int(input("How many quick picks? "))
-print(number_of_picks)
+def generate_quick_pick():
+    quick_pick = []
+    while len(quick_pick) < NUMBERS_PER_LINE:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in quick_pick:
+            quick_pick.append(number)
+    quick_pick.sort()
+    return quick_pick
+
+def main():
+    number_of_picks = int(input("How many quick picks? "))
+    for number in range (number_of_picks):
+        quick_pick = generate_quick_pick()
+        print(" ".join(f"{num:2}" for num in quick_pick))
+
+main()
 
