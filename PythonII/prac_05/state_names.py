674006500cd77bb04efc73ae1848492b446e109f
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-23 19:38:40 +1000

    noting down testing loop issues

--- PythonII/prac_05/state_names.py
+++ PythonII/prac_05/state_names.py
@@ -4,6 +4,8 @@ State names in a dictionary
 File needs reformatting
 """
 
+# if/else loop breaks pretty quickly - will need to fix
+
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {
     "QLD":"Queensland",
