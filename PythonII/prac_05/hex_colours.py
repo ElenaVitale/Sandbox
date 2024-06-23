d7fa4b3ec7dd07535e80e36e50a9e95a39546641
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-23 19:46:29 +1000

    created COLOUR_CODES dictionary

---
+++ PythonII/prac_05/hex_colours.py
@@ -0,0 +1,28 @@
+"""Based on the state name example program above, create a program that allows you
+to look up hexadecimal colour codes like those at http://www.color-hex.com/color-names.html
+
+Use a constant dictionary of about 10 colour names and write a program that allows a user
+to enter a name and get the code, e.g., entering AliceBlue (or aliceblue - make it case-independent)
+should show #f0f8ff.
+
+Entering an invalid colour name should not crash the program.
+Allow the user to enter names until they enter a blank one to stop the loop.
+
+Note: We have just done two exercises that use dictionaries that are constants and named in ALL_CAPS.
+Please don't think this is any kind of rule or pattern.
+Dictionaries that change would not be constants."""
+
+COLOUR_CODES = {
+    'aliceblue': 'f0f8ff',
+    'amaranth': 'e52b50',
+    'amber': 'ffbf00',
+    'amethyst': '9966cc',
+    'antiquewhite': 'faebd7',
+    'apricot': 'fbceb1',
+    'aqua': '00ffff',
+    'asparagus': '87a96b',
+    'beaver': '9f8170',
+    'black': '000000',
+}
+
+print(COLOUR_CODES)
