0e375c8b1872ae2d77f94e3e82d92775a978d4c7
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-23 21:52:25 +1000

    work out basic format, create dictionary

---
+++ PythonII/prac_05/word_occurences.py
@@ -0,0 +1,20 @@
+"""
+Word Occurrences
+Estimate: 40 minutes
+Actual:   xx minutes
+(Estimated time to complete: 30 minutes)
+"""
+"""
+File: word_occurrences.py
+This program counts the occurrences of words in a user-provided string and prints the counts sorted alphabetically.
+It formats the output so that the numbers are aligned in a nice column.
+"""
+
+# create a dictionary
+word_counts = {}
+# get text from the user
+# count, sort the word occurances
+# format text to look neat
+# print result
+
+
