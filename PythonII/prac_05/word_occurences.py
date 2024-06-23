58e7f86060c3695a7f6462107fb4a66fe4c26412
Elena Vitale <elena.vitalie@my.jcu.edu.au>
2024-06-23 22:09:28 +1000

    main block, refactor, fix errors

--- PythonII/prac_05/word_occurences.py
+++ PythonII/prac_05/word_occurences.py
@@ -12,25 +12,36 @@ It formats the output so that the numbers are aligned in a nice column.
 
 # create a dictionary
 word_counts = {}
-# get text from the user
-words = input("Text: ").lower()
-print(words)
 
 
-# count, sort the word occurences
-longest_word_length = 0
-for word in sorted_word_counts:
+def find_longest_word(sorted_word_counts):
+    longest_word_length = 0
+    for word in sorted_word_counts:
+        if len(word) > longest_word_length:
+            longest_word_length = len(word)
+    return longest_word_length
 
 
-for word in words:
-    if word in word_counts:
-        word_counts[word] += 1
-    else:
-        word_counts[word] = 1
-# print (word_counts[word])
+def count_word_occurence(words):
+    for word in words:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+def print_word_count(sorted_word_counts, longest_word_length):
+    for word, count in sorted_word_counts.items():
+        print(f"{word:{longest_word_length}} : {count}")
 
 
-# format text to look neat
-# print result
+def main():
+    words = input("Text: ").lower().split()
+    count_word_occurence(words)
+    sorted_word_counts = dict(sorted(word_counts.items()))
+    longest_word_length = find_longest_word(sorted_word_counts)
+    print_word_count(sorted_word_counts, longest_word_length)
+
+
+main()
 
 
